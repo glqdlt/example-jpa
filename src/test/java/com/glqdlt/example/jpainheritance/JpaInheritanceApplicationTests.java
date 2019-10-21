b27bcce35d19b71d9868315adde4e21bcc893495
jhun <jhun@nm-4ones.com>
2019-10-21 12:31:41 +0900

    Init

---
+++ src/test/java/com/glqdlt/example/jpainheritance/JpaInheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glqdlt.example.jpainheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaInheritanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
